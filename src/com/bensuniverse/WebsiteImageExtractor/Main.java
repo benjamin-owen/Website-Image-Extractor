1f1f2dfe1bcf4aacc5e1d6bb2ced7fc9b5c1234c
benjamin-owen <59540110+benjamin-owen@users.noreply.github.com>
2021-05-27 11:54:54 -0400

    Code cleanup and update fields for version 1.1 release

--- src/com/bensuniverse/WebsiteImageExtractor/Main.java
+++ src/com/bensuniverse/WebsiteImageExtractor/Main.java
@@ -2,26 +2,17 @@ package com.bensuniverse.WebsiteImageExtractor;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
-import javax.swing.*;
-
 public class Main {
 
     private final static String NAME = "Website Image Extractor";
-    private final static String VERSION = "1.0";
+    private final static String VERSION = "1.1";
     private final static String AUTHOR = "Benjamin Owen";
-    private final static String DATE = "2021-05-26";
+    private final static String DATE = "2021-05-27";
 
     public static void main(String[] args) {
 
         // set look and feel
         FlatDarkLaf.setup();
-
-        // use system look and feel
-//        try {
-//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
         // initialize JFrame with title passed and set visibility to true
         new MainWindow().setVisible(true);
